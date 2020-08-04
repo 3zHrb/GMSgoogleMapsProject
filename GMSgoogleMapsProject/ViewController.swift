d88840911724a3bf67e2252fedc09839e45dd290
Abdulaziz Alharbi <abdulazizalharbi@Abdulazizs-MacBook-Pro.local>
2020-08-04 18:22:01 +0300

    empty project

---
+++ GMSgoogleMapsProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GMSgoogleMapsProject
+//
+//  Created by Abdulaziz Alharbi on 14/12/1441 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
